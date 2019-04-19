340665eec7c4c05dfbb56f6c7aaf2e8db8fc426a
BalaDhruv <bala@hexolabs.com>
2019-04-19 17:13:43 +0530

    updated.

--- android/app/src/main/java/com/example/qr_reader_example/MainActivity.java
+++ android/app/src/main/java/com/example/qr_reader_example/MainActivity.java
@@ -8,13 +8,21 @@ import android.os.Bundle;
 import android.util.Base64;
 import android.util.Base64OutputStream;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.CharBuffer;
 import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 import java.nio.charset.CharsetEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import java.util.Map;
@@ -30,7 +38,7 @@ public class MainActivity extends FlutterActivity {
     GeneratedPluginRegistrant.registerWith(this);
 
     new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(new MethodChannel.MethodCallHandler() {
-      @TargetApi(Build.VERSION_CODES.FROYO)
+      @TargetApi(Build.VERSION_CODES.KITKAT)
       @Override
       public void onMethodCall(MethodCall methodCall, MethodChannel.Result result) {
 
@@ -39,26 +47,59 @@ public class MainActivity extends FlutterActivity {
         if (methodCall.method.equals("decodeQr")) {
           System.out.println("INSIDE DECODE QR ---------------------------------------------------------");
           String message = arguments.get("data").toString();
+          Charset utf8charset = Charset.forName("UTF-8");
           Charset iso88591charset = Charset.forName("ISO-8859-1");
-          try {
-            ByteBuffer b = ByteBuffer.wrap(message.getBytes());
-            CharBuffer outputBuffer = iso88591charset.decode(b);
+//          ByteBuffer bb = null;
+//          bb = ByteBuffer.wrap(message.getBytes(StandardCharsets.ISO_8859_1)).order(ByteOrder.LITTLE_ENDIAN);
+////          bb.order(ByteOrder.LITTLE_ENDIAN);
+//          System.out.println(Arrays.toString(bb.array()));
+//            try {
+//                byte[] bytes = message.getBytes("ISO-8859-1");
+//                for(int i:bytes){
+//                  System.out.println(i);
+//                }
+//            } catch (UnsupportedEncodingException e) {
+//                e.printStackTrace();
+//            }
 
-            CharsetDecoder encoder=iso88591charset.newDecoder();
-            CharBuffer a = encoder.decode(b);
-            System.out.println(Base64.encodeToString(message.getBytes("ISO-8859-1"),Base64.DEFAULT));
-            System.out.println(Arrays.toString(outputBuffer.array()));
-            System.out.println(Arrays.toString(message.getBytes("UTF-8")));
-            System.out.println(Arrays.toString(message.getBytes("ISO-8859-1")));
-            System.out.println(Arrays.toString(message.getBytes("US-ASCII")));
-            System.out.println(Arrays.toString(message.getBytes("UTF-16BE")));
-            System.out.println(Arrays.toString(message.getBytes("UTF-16LE")));
-            System.out.println(Arrays.toString(message.getBytes("UTF-16")));
+            // Charset utf8 = Charset.forName("UTF-8");
+          // byte[] data = new byte[message.size()];
+          // for (int i = 0; i < data.length; i++) {
+          //   data[i] = (byte) message.get(i);
+          //   System.out.println(data[i]);
+          // }
+//          System.out.println(new String(message, utf8));
+          // try {
+          ByteBuffer b = null;
+          try {
+            b = ByteBuffer.wrap(message.getBytes("ISO-8859-1"));
+//            String stttr = new String(message.getBytes("ISO-8859-1"),iso88591charset);
+//            System.out.println(stttr);
           } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
-          } catch (CharacterCodingException e) {
-            e.printStackTrace();
           }
+          b.order(ByteOrder.LITTLE_ENDIAN);
+             CharBuffer outputBuffer = utf8charset.decode(b);
+
+          //   CharsetDecoder encoder=iso88591charset.newDecoder();
+          //   CharBuffer a = encoder.decode(b);
+          //   System.out.println(Base64.encodeToString(message.getBytes("ISO-8859-1"),Base64.DEFAULT));
+             System.out.println(Arrays.toString(outputBuffer.array()));
+          for(int i:outputBuffer.array()){
+                  System.out.println((byte)i & 0xff);
+                }
+          System.out.println((byte)outputBuffer.charAt(0));
+          //   System.out.println(Arrays.toString(message.getBytes("UTF-8")));
+          //   System.out.println(Arrays.toString(message.getBytes("ISO-8859-1")));
+          //   System.out.println(Arrays.toString(message.getBytes("US-ASCII")));
+          //   System.out.println(Arrays.toString(message.getBytes("UTF-16BE")));
+          //   System.out.println(Arrays.toString(message.getBytes("UTF-16LE")));
+          //   System.out.println(Arrays.toString(message.getBytes("UTF-16")));
+          // } catch (UnsupportedEncodingException e) {
+          //   e.printStackTrace();
+          // } catch (CharacterCodingException e) {
+          //   e.printStackTrace();
+          // }
         }
 
       }
--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:qr/qr.dart';
 import 'package:qr_reader/qr_reader.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:flutter/services.dart';
+
 void main() {
   runApp(new MyApp());
 }
@@ -37,19 +38,26 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void initState() {
-    iso = 'AU6PYJROj0gAkABEAAACynx6v2F9RwAAAAAUAAEAAAA=';
+    iso = 'AU6PXA5Oj0PAm8BEAAACynx6z3lcpQAAAAACAAIAAAA=';
     var ba64 = base64.decode(iso);
     print(ba64);
+    // print(ascii.decode(ba64));
+    // var byte = utf8.encode(iso);
+    // print(byte);
+    // // getData(ba64);
     iso = latin1.decode(ba64);
     print(iso);
     super.initState();
   }
 
-  getData(String code) async{
+  getData(String code) async {
     print(code);
     const platform = const MethodChannel('com.rq-exam/decode');
-    var result = await platform
-        .invokeMethod('decodeQr', {'data': code});
+    var result = await platform.invokeMethod('decodeQr', {'data': code});
+  }
+
+  getUnicode(int code) {
+    print(code.toRadixString(code));
   }
 
   @override
@@ -59,39 +67,52 @@ class _MyHomePageState extends State<MyHomePage> {
         title: const Text('QR Reader'),
       ),
       body: new Center(
-          child: new FutureBuilder<String>(
-              future: _barcodeString,
-              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                if (snapshot.data != null) {
-                  print(
-                      '----------------------------------------------------------');
-                  print(
-                      '----------------------------------------------------------');
-                  // setState(() {});
-                  print(snapshot.data);
-//                  getData(snapshot.data);
-                  // var utf8st = utf8.encode(snapshot.data);
-                  // print(utf8st);
-                  var utf8Str = latin1.encode('\u0001N`NH\u0000\u0000D\u0000\u0000\u0002Ê|z¿a}G\u0000\u0000\u0000\u0000\u0014\u0000\u0001\u0000\u0000\u0000');
-                  print(utf8Str);
-                  var base64Str = base64.encode(utf8Str);
-                  print(base64Str);
-                  // var iso = latin1.decode(base64Str, allowInvalid: true);
-                  // print(iso);
-                  print(
-                      '----------------------------------------------------------');
-                  print(
-                      '----------------------------------------------------------');
-                }
-                return snapshot.data != null
-                    ? Text(snapshot.data)
-                    : new QrImage(
-                        version: 4,
-                        data: '\u0001N`NH\u0000\u0000D\u0000\u0000\u0002Ê|z¿a}G\u0000\u0000\u0000\u0000\u0014\u0000\u0001\u0000\u0000\u0000',
-                        size: 200.0,
-                  errorCorrectionLevel: QrErrorCorrectLevel.L,
-                      );
-              })),
+        child: new FutureBuilder<String>(
+          future: _barcodeString,
+          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+            if (snapshot.data != null) {
+              print(
+                  '----------------------------------------------------------');
+              print(
+                  '----------------------------------------------------------');
+              // setState(() {});
+              print(snapshot.data);
+
+              var lat = Latin1Codec(allowInvalid: true);
+              print(lat.encode('ÿþýü'));
+              print('-----------------------------------' +
+                  lat.encode(snapshot.data).toString());
+              getData(snapshot.data);
+
+//               var utf8st = utf8.encode(snapshot.data);
+//               print(utf8st);
+//               utf8st.forEach((byte){
+//
+//                 print(byte&0xFF);
+//               });
+//               var utf8Str = latin1.encode(snapshot.data);
+//               print(utf8Str);
+              // var base64Str = base64.encode(utf8Str);
+              // print(base64Str);
+              // var iso = latin1.decode(base64Str, allowInvalid: true);
+              // print(iso);
+              print(
+                  '----------------------------------------------------------');
+              print(
+                  '----------------------------------------------------------');
+            }
+
+            return snapshot.data != null
+                ? Text(snapshot.data)
+                : new QrImage(
+                    // version: 4,
+                    data: iso,
+                    size: 200.0,
+                    // errorCorrectionLevel: QrErrorCorrectLevel.L,
+                  );
+          },
+        ),
+      ),
       floatingActionButton: new FloatingActionButton(
         onPressed: () {
           setState(() {
